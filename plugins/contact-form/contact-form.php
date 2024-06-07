9f2e110fb6c9145de8bae93567ffcfc2d1a95176
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-06-07 23:39:36 +0530

    Add files via upload

---
+++ plugins/contact-form/contact-form.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Contact Form
+ * Description: Sample Contact form
+ * Author: Dhruv Pawar
+ * Text Domain: Sample-contact-form
+ * 
+ */
+
+ if(!defined('ABSPATH')){
+    exit;
+ }
+?>
