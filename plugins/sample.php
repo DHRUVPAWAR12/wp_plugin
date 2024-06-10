3735b6bd9dfb37436a3f20d1ac4f3ac72b5ac3a3
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-06-10 16:46:46 +0530

    Update sample.php

--- plugins/sample.php
+++ plugins/sample.php
@@ -6,6 +6,8 @@ Plugin Name: Sample plugin
 Description: Amazing plugin
 Version: 1.0
 Author: Dhruv Pawar
+Text Domain: wcpdomain
+Domain Path: /languages
 */
 
 
@@ -15,12 +17,56 @@ class WordCountAndTimePlugin{
     {
         add_action('admin_menu',array($this,'adminPage'));
         add_action('admin_init',array($this,'settings'));
+        add_filter('the_content',array($this,'ifWrap'));
+        add_action('init',array($this,'languages'));
     }    
+
+    function languages(){
+        load_plugin_textdomain('wcpdomain', false,dirname(plugin_basename(__FILE__)). '/languages');
+    }
+
+    function ifWrap($content){
+        if((is_main_query() AND is_single())AND
+        (
+        get_option('wcp_wordcount','1')OR 
+        get_option('wcp_charactercount','1') OR 
+        get_option('wcp_readtime','1')
+        )) {
+                return $this ->createHTML($content);
+        }
+        return $content;
+    }
+
+    function createHTML($content){
+            $html = '<h3>' .esc_html( get_option('wcp_headline','Post Statistics')).'</h3><p>';
+
+            if(get_option('wcp_wordcount','1') OR get_option('wcp_readtime','1')){
+                $wordCount = str_word_count(strip_tags($content));
+            }
+
+            if(get_option('wcp_wordcount','1')){
+                $html .= __('This post has','wcpdomain').' ' . $wordCount . ' '.__('words', 'wcpdomain').'<br>';
+            }
+
+            if(get_option('wcp_charactercount','1')){
+                $html .= 'This post has ' .strlen(strip_tags($content)) . ' characters.<br>';
+            }
+
+            if(get_option('wcp_readtime','1')){
+                $html .= 'This post will take about ' .round($wordCount/225) . ' minutes(s) to read.<br>';
+            }
+                $html .= '</p>';
+
+            if(get_option('wcp_location','0') == '0'){
+                return $html . $content;
+            }
+            return $content. $html;
+    }
     function settings(){
 
         add_settings_section('wcp_first_section',null,null,'word-count-settings-page');
         add_settings_field('wcp_location','Display location',array($this,'locationHTML'),'word-count-settings-page','wcp_first_section');
-        register_setting('wordcountplugin','wcp_location',array('sanatize_callback' => 'sanatize_text_field' , 'default' =>'0'));
+        register_setting('wordcountplugin','wcp_location',array('sanatize_callback' => array($this, 'sanatizeLocation') , 'default' =>'0'));
 
         add_settings_field('wcp_headline','Headline Text',array($this,'headlineHTML'),'word-count-settings-page','wcp_first_section');
         register_setting('wordcountplugin','wcp_headline',array('sanatize_callback' => 'sanatize_text_field' , 'default' =>'Post Statistic'));
@@ -35,6 +81,13 @@ class WordCountAndTimePlugin{
         register_setting('wordcountplugin','wcp_readtime',array('sanatize_callback' => 'sanatize_text_field' , 'default' =>'Post Statistic'));
 
     }
+    function sanatizeLocation($input){
+        if($input !='0' AND $input != '1'){
+            add_settings_error('wcp_location','wcp_location_error','Display location must be either beginning or end.');
+            return get_option('wcp_location');
+        }
+        return $input;
+    }
 
     function wordcountHTML(){ ?>
         <input type="checkbox" name="wcp_wordcount" value="1" <?php checked(get_option('wcp_wordcount'), '1')?>>
@@ -42,7 +95,7 @@ class WordCountAndTimePlugin{
     
 
     function charactercountHTMl(){ ?>
-        <input type="checkbox" name="wcp_charactercount" value="1" <?php checked(get_option('wcp_charcount'), '1')?> >
+        <input type="checkbox" name="wcp_charactercount" value="1" <?php checked(get_option('wcp_charactercount'), '1')?> >
     <?php }
     
     function readtimeHTML(){ ?>
@@ -62,7 +115,7 @@ class WordCountAndTimePlugin{
 
 
     function adminPage(){
-        add_options_page('Word Count Settings' , 'Word Count' , 'manage_options' , 'word-count-settings-page',array($this,'ourHTML'));
+        add_options_page('Word Count Settings' , __('Word Count','wcpdomain') , 'manage_options' , 'word-count-settings-page',array($this,'ourHTML'));
     }
     function ourHTML(){?>
         <div class="wrap">
