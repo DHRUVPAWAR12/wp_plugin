3e7b7c1b868b5d2fd2afe95046a6485006c4f310
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-06-10 16:47:54 +0530

    Add files via upload

---
+++ plugins/languages/wcpdomain-es_MX.l10n.php
@@ -0,0 +1,4 @@
+<?php
+return ['project-id-version'=>'Sample plugin','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-06-10 10:02+0000','po-revision-date'=>'2024-06-10 10:09+0000','last-translator'=>'','language-team'=>'Spanish (Mexico)','language'=>'es_MX','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.9; wp-6.5.4','x-domain'=>'wcpdomain','messages'=>['Amazing plugin'=>'
+
+','This post has'=>'Esta entrada tiene','Word Count'=>'El recuento de palabras','words'=>'palabras']];
