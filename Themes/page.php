7a3d0fa02b05aaf3f9942c411a6c7d53a9afd02c
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-06-16 23:40:00 +0530

    Update page.php

--- Themes/page.php
+++ Themes/page.php
@@ -1,30 +1,59 @@
 <?php
 
 get_header(); ?>
-	
-	
-	<div class="site-content clearfix">
-		
 
-		<div class="main-column">
-			<?php if (have_posts()) :
-				while (have_posts()) : the_post();
+<div class="site-content clearfix">
 
-				get_template_part('content', 'page');
+  <div class="main-column">
 
-				endwhile;
+    <?php
+    
+    $cat_id = 3;
 
-				else :
-					echo '<p>No content found</p>';
+    
+    $category_posts = new WP_Query(array(
+      'category__in' => $cat_id,
+      'posts_per_page' => 3, // Limit to 3 posts
+    ));
 
-				endif;
-				?>
-		</div>
+    if ($category_posts->have_posts()) :
+      ?>
 
-		<?php get_sidebar(); ?>
-		
-	</div>
-	
-	<?php get_footer();
+      <h2>Latest Posts from "<?php echo get_cat_name($cat_id); ?>" Category</h2>
+
+      <ul class="post-list">
+
+        <?php while ($category_posts->have_posts()) : $category_posts->the_post(); ?>
+
+          <li class="post-item">
+            <a href="<?php the_permalink(); ?>">
+              <h3><?php the_title(); ?></h3>
+              <?php the_excerpt(); ?>
+            </a>
+          </li>
+
+        <?php endwhile; ?>
+
+      </ul>
+
+      <?php echo paginate_links($category_posts); // Pagination if needed ?>
+
+    <?php else : ?>
+
+      <p>No posts found in this category.</p>
+
+    <?php endif;
+
+    
+    wp_reset_postdata();
+    ?>
+
+  </div>
+
+  <?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer();
 
 ?>
